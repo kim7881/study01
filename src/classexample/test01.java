c0dd6e7ea81fb4963a2cb8c1f1523333274c6c34
kim7881 <rlawls7881@gmail.com>
2021-08-05 13:28:56 +0900

    test2

--- src/classexample/test01.java
+++ src/classexample/test01.java
@@ -18,7 +18,9 @@ class Tv{
 	boolean power;		// 전원상태(on/off)
 	int channel;		// 채널
 	
+	
 	// Tv의 기능(메서드)
+	// tv
 	void power()	{power = !power;}		// Tv를 켜거나 끄는 기능을 하는 메서드
 	void channelUp()	{ ++channel; }		// Tv의 채널을 높이는 기능을 하는 메서드
 	void channelDown()	{ --channel; }		// Tv의 채널을 낮추는 기능을 하는 메서드
